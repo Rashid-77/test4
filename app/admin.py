ef2e471610583fee0ad8060916096debc0243f20
Rashid <rash.sh.77@gmail.com>
2020-08-17 00:44:47 +0500

    облегчил восприятие кода препятствующего попытки записи дубликата продукта в админке. А также уменньшил количество запросов к бд там же

--- app/admin.py
+++ app/admin.py
@@ -17,54 +17,39 @@ class SupplierProductAdmin(admin.ModelAdmin):
     list_display = ('supplier', 'product', 'product_price', 'availability')
 
     def delete_last_msg(self, request):
-        '''
+        """
         The only purpose to suppress the "success" message after field validation
         In this case only message queue has one or two messages
         if one then it is only SUCCESS
         if two then my message(ERROR or INFO) and default SUCCESS
-        '''
+        """
         storage = messages.get_messages(request)
         if len(storage._queued_messages) > 1:
             del storage._queued_messages[-1]
 
-    def is_vendor_code_unique(self, obj):
-        '''
-        For new and editing existing SupplierProduct
-        '''
-        this_suppliers_products = SupplierProduct.objects.filter(supplier__exact=obj.supplier)
-        for sp in this_suppliers_products:
-            if sp.product == obj.product:
-                return False
-        return True
-
     def save_model(self, request, obj, form, change):
-        '''
-        Save new product(price) for this supplier if vendor code is unique for this supplier
-        '''
-        pass
-        msg_dict = {
-            'name': obj.supplier,
-            'code': obj.product,
-        }
-        if self.is_vendor_code_unique(obj):
+        """
+        1. Save new product if vendor code is unique for this supplier
+        2. User can edit existing SupplierProduct by altering supplier field or product field, so vendor code
+            became not unique. Шn this case saving is not allowed
+        """
+        if not SupplierProduct.objects.filter(supplier__exact=obj.supplier, product__exact=obj.product).exists():
             obj.save()
-        elif not change:
-            msg = _('The {name} already has a product with this vendor code "{code}". Try another')
-            self.message_user(request, format_html(msg, **msg_dict), level=messages.ERROR)
-        else:
-            old = SupplierProduct.objects.get(pk=obj.id)
-            if old.supplier != obj.supplier or old.product != obj.product:
-                msg = _('The {name} already has a product with this vendor code "{code}". Try another')
-                self.message_user(request, format_html(msg, **msg_dict), level=messages.ERROR)
-            else:
+            return
+        elif change:
+            # if user didn't change supplier or product fieds, so save
+            if SupplierProduct.objects.filter(id=obj.id, supplier_id=obj.supplier, product_id=obj.product):
                 obj.save()
+                return
+        msg_dict = {'name': obj.supplier, 'code': obj.product}
+        msg = _('The {name} already has a product with this vendor code "{code}". Try another')
+        self.message_user(request, format_html(msg, **msg_dict), level=messages.ERROR)
 
     def response_post_save_add(self, request, obj):
         """
         Figure out where to redirect after the 'Save' button has been pressed
         when adding a new object.
         """
-        print('response_post_save_add----------')
         self.delete_last_msg(request)
         return self._response_post_save(request, obj)
 
@@ -73,7 +58,6 @@ class SupplierProductAdmin(admin.ModelAdmin):
         Figure out where to redirect after the 'Save' button has been pressed
         when editing an existing object.
         """
-        print('response_post_save_change----------')
         self.delete_last_msg(request)
         return self._response_post_save(request, obj)
 
