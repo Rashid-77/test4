d75d624a7119823371a5d4ea490ac053031abba5
Rashid <rash.sh.77@gmail.com>
2020-08-20 23:45:57 +0500

    переделал функцию suppliers и suppliers.html

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.db.models import Q, OuterRef, Exists, Value
+from django.db.models import Q, OuterRef, Exists
 from django.core.exceptions import PermissionDenied
 from .models import Product, Image, Supplier
 
@@ -35,10 +35,6 @@ def my_product(request):
     '''
     user = request.user
     if user.groups.filter(name='Suppliers').exists():
-    #     my_products = SupplierProduct.objects.\
-    #         filter(supplier__name=user).order_by('product'). \
-    #         values('product__name', 'product__product', 'product_price', 'availability', 'product__main_image')
-
         my_products = Product.objects.filter(supplier__name=user).order_by('vendor_id'). \
             values('name', 'vendor_id', 'price', 'availability', 'main_image')
         dataset = {'my_products': my_products}
@@ -53,21 +49,20 @@ def suppliers(request):
     This view func makes a table of cheaper available products then products of currently logged in supplier user
     '''
     user = request.user
-    # if user.groups.filter(name='Suppliers').exists():
-        # cheaper_products = SupplierProduct.objects.annotate(
-        #     exist=Exists(SupplierProduct.objects.filter(
-        #         Q(supplier__name=user),
-        #         ~Q(supplier_id=OuterRef('supplier_id')),
-        #         availability=True,
-        #         product_id=OuterRef('product_id'),
-        #         product_price__gt = OuterRef('product_price')
-        #         )
-        #     )
-        # ).filter(availability=True, exist=True).order_by('product', 'product_price'). \
-        #     values('product__name', 'product__product', 'supplier__name', 'product_price')
-        # return render(request, 'app/suppliers.html', {'cheaper_products': cheaper_products})
-    # else:
-    #     raise PermissionDenied
+    if user.groups.filter(name='Suppliers').exists():
+        cheaper_products = Product.objects.annotate(
+            exist=Exists(Product.objects.filter(
+                Q(supplier__name=user),
+                ~Q(supplier_id=OuterRef('supplier_id')),
+                vendor_id=OuterRef('vendor_id'),
+                price__gt= OuterRef('price')
+                )
+            )
+        ).filter(availability=True, exist=True).values('name', 'vendor_id', 'supplier__name', 'price')
+        dataset = {'cheaper_products': cheaper_products}
+        return render(request, 'app/suppliers.html', {'cheaper_products': cheaper_products})
+    else:
+        raise PermissionDenied
 
 
 @login_required
@@ -80,14 +75,6 @@ def buyer(request):
         available_product = Product.objects.filter(availability__exact='True'). \
             order_by('vendor_id', 'price'). \
             values('id', 'name', 'vendor_id', 'supplier__name', 'price', 'main_image')
-        # print('---------')
-        # for i in available_product:
-        #     print(i['id'], i['name'], i['main_image'])
-        #
-        # im = Image.objects.all()
-        # print('----all_images----')
-        # for i in im:
-        #     print(i.id, i.path, i.order)
         dataset = {'available_product': available_product}
         return render(request, 'app/buyer.html', dataset)
     else:
