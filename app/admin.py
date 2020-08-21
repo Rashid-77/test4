1fb25ed9c38a6085c7c36380b7eeaab637b80a72
Rashid <rash.sh.77@gmail.com>
2020-08-21 14:40:15 +0500

    упростил исключение дубликатов vendor_id у поставщика

--- app/admin.py
+++ app/admin.py
@@ -5,64 +5,15 @@ from django.utils.translation import gettext_lazy as _
 from .models import Product, Image, Supplier
 
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'vendor_id', 'supplier', 'price', 'availability')
-
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('path', 'order')
 
 
-# class SupplierProductAdmin(admin.ModelAdmin):
-#     list_display = ('supplier', 'product', 'product_price', 'availability')
-#
-#     def delete_last_msg(self, request):
-#         """
-#         The only purpose to suppress the "success" message after field validation
-#         In this case only message queue has one or two messages
-#         if one then it is only SUCCESS
-#         if two then my message(ERROR or INFO) and default SUCCESS
-#         """
-#         storage = messages.get_messages(request)
-#         if len(storage._queued_messages) > 1:
-#             del storage._queued_messages[-1]
-#
-#     def save_model(self, request, obj, form, change):
-#         """
-#         1. Save new product if vendor code is unique for this supplier
-#         2. User can edit existing SupplierProduct by altering supplier field or product field, so vendor code
-#             became not unique. Шn this case saving is not allowed
-#         """
-        # if not SupplierProduct.objects.filter(supplier__exact=obj.supplier, product__exact=obj.product).exists():
-        #     obj.save()
-        #     return
-        # elif change:
-            # if user didn't change supplier or product fieds, so save
-            # if SupplierProduct.objects.filter(id=obj.id, supplier_id=obj.supplier, product_id=obj.product):
-            #     obj.save()
-            #     return
-        # msg_dict = {'name': obj.supplier, 'code': obj.product}
-        # msg = _('The {name} already has a product with this vendor code "{code}". Try another')
-        # self.message_user(request, format_html(msg, **msg_dict), level=messages.ERROR)
-
-    # def response_post_save_add(self, request, obj):
-    #     """
-    #     Figure out where to redirect after the 'Save' button has been pressed
-    #     when adding a new object.
-    #     """
-    #     self.delete_last_msg(request)
-        # return self._response_post_save(request, obj)
-    #
-    # def response_post_save_change(self, request, obj):
-    #     """
-    #     Figure out where to redirect after the 'Save' button has been pressed
-    #     when editing an existing object.
-    #     """
-    #     self.delete_last_msg(request)
-        # return self._response_post_save(request, obj)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'vendor_id', 'supplier', 'price', 'availability', 'main_image')
 
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Supplier)
-# admin.site.register(SupplierProduct, SupplierProductAdmin)
 admin.site.register(Image, ImageAdmin)
--- app/models.py
+++ app/models.py
@@ -3,20 +3,20 @@ from django.db import models
 
 
 class Image(models.Model):
-    '''
+    """
     Here is the other product images.
-    '''
+    """
     path = models.ImageField(default=None, blank=False)
     order = models.PositiveIntegerField('order', default=0)
-    # product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+
     def __str__(self):
         return '{0} {1}'.format(self.path, self.order)
 
 
 class Product(models.Model):
-    '''
+    """
     Product description with main image
-    '''
+    """
     name = models.CharField('product name', max_length=50)
     vendor_id = models.CharField('vendor code', default=None, max_length=50)
     supplier = models.ForeignKey('Supplier', default=None, on_delete=models.CASCADE)
@@ -25,14 +25,17 @@ class Product(models.Model):
     main_image = models.ImageField('Main image', default='no_image_available-1.png', blank=False)
     image = models.ManyToManyField(Image, blank=True, related_name='products', verbose_name='Additional images')
 
+    class Meta:
+        unique_together = [['supplier', 'vendor_id']]
+
     def __str__(self):
         return '{0}'.format(self.name, self.price, self.availability)
 
 
 class Supplier(models.Model):
-    '''
+    """
     All information about supplier are here
-    '''
+    """
     name = models.CharField('Supplier name', default='', max_length=50)
     # telephone = models.CharField('Telephone', default=None, max_length=12)
     zip_code = models.CharField('zip code', default=None, max_length=6)
@@ -40,15 +43,3 @@ class Supplier(models.Model):
     def __str__(self):
         return self.name
 
-
-# class SupplierProduct(models.Model):
-#     '''
-#     Here is information about what products the supplier has and how much they cost
-#     '''
-#     supplier = models.ForeignKey(Supplier, default=None, on_delete=models.CASCADE)
-#     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
-#     product_price = models.DecimalField('Price', default=None, max_digits=11, decimal_places=2)
-#     availability = models.BooleanField(default=False)
-#
-#     def __str__(self):
-#         return '{0}'.format(self.supplier)
